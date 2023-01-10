061c5493f0e4e9282171c4ff57507142d2c38a37
HHassanzada <tring.regod786@gmail.com>
2023-01-10 17:44:04 +0300

    Add Lines

--- Exercises/All.cs
+++ Exercises/All.cs
@@ -135,6 +135,13 @@ namespace Exercises
 
 
 
+
+
+
+
+
+
+
             public override string ToString()
             {
                 return $"Id: {Id}, Name: {Name}, Type: {PetType}, Weight: {Weight}";
